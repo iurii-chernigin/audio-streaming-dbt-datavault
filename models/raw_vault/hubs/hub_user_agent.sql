3e096fc10809469988d9892eed282722ad4947a7
chernigin.y@gmail.com <chernigin.y@gmail.com>
2023-04-22 20:05:00 +0000

    raw-vault: extend source model with auth events (fix mistake)

--- models/raw_vault/hubs/hub_user_agent.sql
+++ models/raw_vault/hubs/hub_user_agent.sql
@@ -1,6 +1,6 @@
-{% set source_model = 'v_stage_listen_events' %}
+{% set source_model = ['v_stage_listen_events', 'v_stage_auth_events'] %}
 {% set src_pk = 'user_agent_pk' %}
-{% set src_nk = ['v_stage_listen_events', 'v_stage_auth_events'] %}
+{% set src_nk = 'user_agent' %}
 {% set src_ldts = 'load_date' %}
 {% set src_source = 'record_source' %}
 
