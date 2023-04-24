da37851ab74f3d88a31bd5efd5e74c335ee94c3e
chernigin.y@gmail.com <chernigin.y@gmail.com>
2023-04-24 21:03:46 +0000

    datamart-song-play: add sub-mart with hourly metrics (uniq users, songs, etc)

---
+++ models/datamarts/mart_song_play_hourly_metrics.sql
@@ -0,0 +1,13 @@
+
+select 
+
+    date(play_start_ts) as play_date,
+    extract(hour from play_start_ts) as play_hour,
+
+    count(distinct user_id)                             as uniq_users,
+    count(distinct concat(song_name, song_artist))      as uniq_songs,
+    count(distinct user_agent)                          as uniq_user_agents
+
+from {{ ref('mart_song_play') }}
+
+group by play_date, play_hour
